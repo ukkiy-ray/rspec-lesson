adcae238c8b018eecb75d8786d4750633a7913e3
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-06 20:56:16 +0900

    新規投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :image)
+  end
 end
