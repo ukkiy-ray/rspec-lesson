075eb6b91249b647bf1ffb2e0843415577e6e569
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-08 20:33:00 +0900

    投稿にユーザーを紐づける機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :description, :image)
+    params.require(:post).permit(:title, :description, :image).merge(user_id: current_user.id)
   end
 
   def set_post
