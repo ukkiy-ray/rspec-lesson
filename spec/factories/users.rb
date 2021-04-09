dde21efdb860981b30cd260dfacdcd980fb22778
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-09 16:25:54 +0900

    Factoriesの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {"0a1b2c3d"}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     birthday              {Faker::Date.between(from: '1930-01-01', to: '2014-12-31')}
   end
