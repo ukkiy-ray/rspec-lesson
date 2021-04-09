ffa2ec6f4169868e0679a2d70977ff6da16ae644
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-09 13:39:33 +0900

    faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    birthday              {'2000-01-01'}
+    birthday              {Faker::Date.between(from: '1930-01-01', to: '2014-12-31')}
   end
 end
