5a5303cd311d8a3bb6b0e5247c88a49da73129d6
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-13 22:22:37 +0900

    ユーザー新規登録の異常系結合テスト実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe "ユーザー新規登録", type: :system do
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
       fill_in 'Password confirmation', with: @user.password_confirmation
-      # fill_in 'Birthday', with: @user.birthday
       select '1930',from: 'user[birthday(1i)]'
       select '12',from: 'user[birthday(2i)]'
       select '12',from: 'user[birthday(3i)]'
@@ -38,11 +37,25 @@ RSpec.describe "ユーザー新規登録", type: :system do
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign up')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Nickname', with: ''
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      fill_in 'Password confirmation', with: ''
+      select '2000',from: 'user[birthday(1i)]'
+      select '11',from: 'user[birthday(2i)]'
+      select '11',from: 'user[birthday(3i)]'
       # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq user_registration_path
     end
   end
 end
