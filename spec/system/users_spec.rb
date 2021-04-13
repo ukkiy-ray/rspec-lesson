0d02a06cc51b7ed93d98429d326d4b6d98f1a695
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-13 22:16:37 +0900

    ユーザー新規登録の結合テスト実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,13 +7,32 @@ RSpec.describe "ユーザー新規登録", type: :system do
   context 'ユーザー新規登録ができるとき' do 
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign up')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Nickname', with: @user.nickname
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password_confirmation
+      # fill_in 'Birthday', with: @user.birthday
+      select '1930',from: 'user[birthday(1i)]'
+      select '12',from: 'user[birthday(2i)]'
+      select '12',from: 'user[birthday(3i)]'
+      
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
       # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
       # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      expect(page).to have_content('Sign out')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('Sign up')
+      expect(page).to have_no_content('Sign in')
     end
   end
   context 'ユーザー新規登録ができないとき' do
