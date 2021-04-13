6c61c992de1d8f32735d24b7ac8c07bc2bd39e0f
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-13 23:40:28 +0900

    Post新規投稿の正常系結合テスト実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'test01test01'}
     password_confirmation {password}
     birthday              {Faker::Date.between(from: '1930-01-01', to: '2014-12-31')}
   end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -3,17 +3,35 @@ require 'rails_helper'
 RSpec.describe "Posts", type: :system do
   before do
     @user = FactoryBot.create(:user)
-    @post = FactoryBot.create(:post)
+    @post = FactoryBot.build(:post)
   end
 
   context '投稿ができるとき'do
     it 'ログインしたユーザーは新規投稿できる' do
       # ログインする
-      # 新規投稿ページへのリンクがあることを確認する
+      visit new_user_session_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
       # 投稿ページに移動する
+      visit new_post_path
       # フォームに情報を入力する
+      fill_in 'post[title]', with: @post.title
+      fill_in 'post[description]', with: @post.description
+
+      # 添付する画像を定義する
+      image_path = Rails.root.join('spec/factories/images/test.png')
+
+      # 画像選択フォームに画像を添付する
+      attach_file('post[image]', image_path, make_visible: true)
+
       # 送信するとPostモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Post.count }.by(1)
       # トップページに遷移することを確認する
+      expect(current_path).to eq(root_path)
       # トップページには先ほど投稿した内容のツイートが存在することを確認する
     end
   end
