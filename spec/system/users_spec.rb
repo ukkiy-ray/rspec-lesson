ca4fb1395d46e9a35ddd3619a14c4c7f38a05ee7
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-13 22:58:26 +0900

    ログイン機能の異常系結合テスト実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -89,11 +89,18 @@ RSpec.describe 'ログイン', type: :system do
   context 'ログインができないとき' do
     it '保存されているユーザーの情報と合致しないとログインができない' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign in')
       # ログインページへ遷移する
+      visit new_user_session_path
       # ユーザー情報を入力する
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
       # ログインボタンを押す
+      find('input[name="commit"]').click
       # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 end
