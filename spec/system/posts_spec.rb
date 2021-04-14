6d504a59aed8b9816625506fb9d7f0d0aa47e502
ukkiy-ray <showy13f207ag@gmail.com>
2021-04-14 19:46:59 +0900

    post編集の正常系テスト実装

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Posts", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @post = FactoryBot.build(:post)
+    sleep(1)
   end
 
   context '投稿ができるとき'do
@@ -51,17 +52,37 @@ RSpec.describe 'Post編集', type: :system do
   before do
     @post1 = FactoryBot.create(:post)
     @post2 = FactoryBot.create(:post)
+    sleep(1)
   end
   context 'Postの編集ができるとき' do
     it 'ログインしたユーザーは自分が投稿したPostの編集ができる' do
       # post1を投稿したユーザーでログインする
-      # post1に「Update」ボタンがあることを確認する
-      # 編集ページへ遷移する
+      visit new_user_session_path
+      fill_in 'Email', with: @post1.user.email
+      fill_in 'Password', with: @post1.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+      # post1の編集ページへ遷移する
+      visit edit_post_path(@post1)
       # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(
+        find('#post_title').value
+      ).to eq(@post1.title)
+      expect(
+        find('#textAreaExample').value
+      ).to eq(@post1.description)
       # 投稿内容を編集する
+      find("#post_title").set("#{@post1.title}+編集したタイトル")
+      find("#textAreaExample").set("#{@post1.description}+編集したテキスト")
       # 編集してもPostモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Post.count }.by(0)
       # トップページに遷移したことを確認する
+      expect(current_path).to eq(root_path)
       # トップページには先ほど変更した内容のPostが存在することを確認する
+      expect(page).to have_content("#{@post1.title}+編集したタイトル")
+      expect(page).to have_content("#{@post1.description}+編集したテキスト")
     end
   end
   context 'Post編集ができないとき' do
